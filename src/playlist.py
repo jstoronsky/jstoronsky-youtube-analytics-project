d3865d89759d921aced34cdb54e84d5f2a76d1ad
Ivan Storonsky <jstoronsky@gmail.com>
2023-05-11 06:24:05 +1200

    hw_15.2 + docs

--- src/playlist.py
+++ src/playlist.py
@@ -13,23 +13,34 @@ class PlayList:
 
     def print_info(self):
         """
-        Выводит в консоль информацию о видосе.
+        Выводит в консоль информацию о плейлисте.
         """
         data_in_dict = self.youtube.playlists().list(id=self.playlist_id, part='snippet').execute()
         return data_in_dict
 
     @property
     def title(self):
+        """
+        Получаем из ранее полученной информации название плейлиста
+        """
         title_ = [tl["snippet"]["title"] for tl in self.print_info()["items"]]
         return title_[0]
 
     @property
     def url(self):
-        url_ = [ur["id"] for ur in self.print_info()["items"]]
-        return f'https://www.youtube.com/playlist?list={url_[0]}'
+        """
+        Формируем ссылку на плейлист
+        """
+        return f'https://www.youtube.com/playlist?list={self.playlist_id}'
 
     @property
     def total_duration(self):
+        """
+        получаем длительность всех видео в плейлисте
+        сначала достаём айди каждого видео в плейлисте, затем по айди вытаскиваем длительность
+        переводим длительность в человеческий формат при помощи isodate.parse_duration
+        затем переводим в общее количество часов при помощи datetime.timedelta
+        """
         content = self.youtube.playlistItems().list(playlistId=self.playlist_id, part='contentDetails').execute()
         video_ids_list = [id_["contentDetails"]["videoId"] for id_ in content["items"]]
         video_info = self.youtube.videos().list(id=video_ids_list, part='contentDetails, statistics').execute()
@@ -39,9 +50,18 @@ class PlayList:
         return total_hours
 
     def show_best_video(self):
+        """
+        получаем видео с наибольшим количеством лайков
+        достаём айди всех видео, по ним получаем лайки у каждого видоса
+        затем получаем индекс видео с самым высоким количеством лайков и формируем ссылку
+        """
         content = self.youtube.playlistItems().list(playlistId=self.playlist_id, part='contentDetails').execute()
         video_ids_list = [id_["contentDetails"]["videoId"] for id_ in content["items"]]
         video_info = self.youtube.videos().list(id=video_ids_list, part='statistics').execute()
-        likes_number = [int(like["statistics"]["viewCount"]) for like in video_info["items"]]
+        likes_number = [int(like["statistics"]["likeCount"]) for like in video_info["items"]]
         best_video_id = video_ids_list[likes_number.index(max(likes_number))]
         return f'https://youtu.be/{best_video_id}'
+
+
+pl = PlayList('PLguYHBi01DWr4bRWc4uaguASmo7lW4GCb')
+print(pl.show_best_video())
